4a5e04c6d2ef115e938caf1968842718f90c0642
KREINS-Lisa-Marie <lisa-marie.kreins@student.hepl.be>
2025-02-03 16:59:59 +0100

    moved Background-images

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const premiereImage = document.querySelector(".backgroundimage:first-of-type");
+
+let position = premiereImage.offsetTop;
+console.log(position);
+
+
+const deuxiemeImage = document.querySelector(".backgroundimage:last-of-type");
+
+let positiondeux = deuxiemeImage.offsetTop;
+console.log(positiondeux);
+
+
+//console.log("hello");  TEST
